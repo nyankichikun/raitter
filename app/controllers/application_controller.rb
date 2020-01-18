156f978a55a9c4996a4eaf05448a4d7b4b6f50c0
nyankichikun <nyan.teacher@gmail.com>
2020-01-19 03:55:02 +0900

    [add]device_auth_tokenを用いた認証機能の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  def authenticate?
+    if invalid_request?
+      render json: { error: 'invalid request'}, status: 401
+    end
+    true
+  end
+
+  private
+
+  def invalid_request?
+    request_headers = DeviceRequestHeaders.new(request)
+    return true if request_headers.invalid?
+
+    user = User.find_by_uid(request_headers.headers["uid"])
+    return true unless user.valid_token?(request_headers.headers["access-token"], request_headers.headers["client"])
+
+    false
+  end
 end
