bb66199954e71a1945380fee33df66afe62fe4d6
nyankichikun <nyan.teacher@gmail.com>
2020-01-18 13:46:19 +0900

    [add]Tweetモデルの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+  has_many :tweets, dependent: :destroy
+
   has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_18_043932) do
+ActiveRecord::Schema.define(version: 2020_01_18_044414) do
+
+  create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.string "content", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
     t.string "email", null: false
@@ -20,4 +28,5 @@ ActiveRecord::Schema.define(version: 2020_01_18_043932) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "tweets", "users"
 end
