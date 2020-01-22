d450c07e43c6dd19a2eacaf8df827139f4e8719c
nyankichikun <nyan.teacher@gmail.com>
2020-01-23 08:26:44 +0900

    [add]DM機能の追加

--- app/controllers/api/v1/rooms_controller.rb
+++ app/controllers/api/v1/rooms_controller.rb
@@ -8,12 +8,27 @@ class Api::V1::RoomsController < ApplicationController
   end
 
   def create
-    begin
-      room = Room.create(room_params)
-      entry = Entry.create({ user_id: current_api_v1_user.id, room_id: room.id })
-      render json: { "room": room, "entry": entry}
-    rescue => e
-      render json: { "error": e  }
+    if private_room?
+      begin
+        raise "already exists" if invalid_private_room_request?(User.find_by_id(params[:user_id]))
+        room = Room.create(room_params)
+        entry1 = Entry.create({ user_id: current_api_v1_user.id, room_id: room.id })
+        entry2 = Entry.create({ user_id: params[:user_id]     , room_id: room.id })
+        return render json: { "room": room }
+      rescue => e
+        return render json: { "error": e  }
+      end
+
+    elsif public_room?
+      begin
+        room = Room.create(room_params)
+        entry = Entry.create({ user_id: current_api_v1_user.id, room_id: room.id })
+        return render json: { "room": room }
+      rescue => e
+        return render json: { "error": e  }
+      end
+    else
+      return render json: { "error": "request is invalid"}
     end
   end
 
@@ -28,15 +43,28 @@ class Api::V1::RoomsController < ApplicationController
 
   private
 
-  def room_params
-    params.permit(:name)
-  end
-
   def set_rooms
     @rooms = current_api_v1_user.rooms
   end
 
   def set_room
     @room = current_api_v1_user.rooms.find_by_id(params[:id])
+  end
+
+  def private_room?
+    params[:user_id].present?
+  end
+
+  def public_room?
+    params[:public] == 'true'
+  end
+
+  def room_params
+    params.permit(:name, :public)
+  end
+
+  def invalid_private_room_request?(user)
+    return true if current_api_v1_user.rooms.where(id: user.rooms).exists?
+    false
   end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,7 @@ class Room < ApplicationRecord
   has_many :messages, dependent: :destroy
   has_many :entries, dependent: :destroy
   has_many :users, through: :entries
+
+  scope :private_rooms, -> { where(public: false) }
+  scope :public_rooms,  -> { where(public: true) }
 end
