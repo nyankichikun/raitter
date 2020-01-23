dff37f0fea326730cee7facb1edc9fb8e0723e7d
nyankichikun <nyan.teacher@gmail.com>
2020-01-23 22:11:29 +0900

    [mod]Tweetのリファクタリング

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -1,9 +1,14 @@
 class Api::V1::TweetsController < ApplicationController
-  before_action :authenticate?, only: [:index, :create]
-  before_action :set_timeline,  only: [:index]
+  before_action :authenticate?, only: [:create]
+  before_action :set_tweet    , only: [:show]
+  before_action :set_timeline,  only: [:index, :show]
 
   def index
     render json: @tweets
+  end
+
+  def show
+    render json: @tweet
   end
 
   def create
@@ -17,6 +22,10 @@ class Api::V1::TweetsController < ApplicationController
   end
 
   private
+
+  def set_tweet
+    @tweet = Tweet.find_by_id(params[:id])
+  end
 
   def set_timeline
     @tweets = Tweet.timeline(current_api_v1_user.following)
